9e31b14a786a97c11965c160c86de3ab6c4071fe
“BrenoMNatal” <breno@ejcm.com.br>
2023-10-15 02:10:58 -0300

    comecin

---
+++ hackathon-hora-do-pesadelo/src/pages/home.tsx
@@ -0,0 +1,313 @@
+
+import banner from "../assets/banner.png"
+
+function Home() {
+    return (
+        <>
+            <main className="pt-4 pb-8 bg-white w-full">
+                <div className="flex w-screen flex-row justify-center">
+                    <img src={banner} className="h-80 rounded-xl" />
+                </div>
+                
+                <div className="flex flex-row flex-nowrap justify-center mt-6 gap-6">
+
+                    <div className="w-16 text-black ease-in duration-200 hover:text-purple-600">
+                        <button className="bg-black w-16 h-16 hover:bg-orange-400 hover:rounded-full transition-transform duration-200 transform hover:scale-90">
+
+                        </button>
+                        <h2 className="text-sm justify-center text-center "> Categoria Categoria</h2>
+                    </div>
+                    <div className="w-16 text-black ease-in duration-200 hover:text-purple-600">
+                        <button className="bg-black w-16 h-16 hover:bg-orange-400 hover:rounded-full transition-transform duration-200 transform hover:scale-90">
+
+                        </button>
+                        <h2 className="text-sm justify-center text-center "> Categoria Categoria</h2>
+                    </div>
+                    <div className="w-16 text-black ease-in duration-200 hover:text-purple-600">
+                        <button className="bg-black w-16 h-16 hover:bg-orange-400 hover:rounded-full transition-transform duration-200 transform hover:scale-90">
+
+                        </button>
+                        <h2 className="text-sm justify-center text-center "> Categoria Categoria</h2>
+                    </div>
+                    <div className="w-16 text-black ease-in duration-200 hover:text-purple-600">
+                        <button className="bg-black w-16 h-16 hover:bg-orange-400 hover:rounded-full transition-transform duration-200 transform hover:scale-90">
+
+                        </button>
+                        <h2 className="text-sm justify-center text-center "> Categoria Categoria</h2>
+                    </div>
+                    <div className="w-16 text-black ease-in duration-200 hover:text-purple-600">
+                        <button className="bg-black w-16 h-16 hover:bg-orange-400 hover:rounded-full transition-transform duration-200 transform hover:scale-90">
+
+                        </button>
+                        <h2 className="text-sm justify-center text-center "> Categoria Categoria</h2>
+                    </div>
+                    <div className="w-16 text-black ease-in duration-200 hover:text-purple-600">
+                        <button className="bg-black w-16 h-16 hover:bg-orange-400 hover:rounded-full transition-transform duration-200 transform hover:scale-90">
+
+                        </button>
+                        <h2 className="text-sm justify-center text-center "> Categoria Categoria</h2>
+                    </div>
+                    <div className="w-16 text-black ease-in duration-200 hover:text-purple-600">
+                        <button className="bg-black w-16 h-16 hover:bg-orange-400 hover:rounded-full transition-transform duration-200 transform hover:scale-90">
+
+                        </button>
+                        <h2 className="text-sm justify-center text-center "> Categoria Categoria</h2>
+                    </div>
+                    <div className="w-16 text-black ease-in duration-200 hover:text-purple-600">
+                        <button className="bg-black w-16 h-16 hover:bg-orange-400 hover:rounded-full transition-transform duration-200 transform hover:scale-90">
+
+                        </button>
+                        <h2 className="text-sm justify-center text-center "> Categoria Categoria</h2>
+                    </div>
+                    <div className="w-16 text-black ease-in duration-200 hover:text-purple-600">
+                        <button className="bg-black w-16 h-16 hover:bg-orange-400 hover:rounded-full transition-transform duration-200 transform hover:scale-90">
+
+                        </button>
+                        <h2 className="text-sm justify-center text-center "> Categoria Categoria</h2>
+                    </div>
+                    <div className="w-16 text-black ease-in duration-200 hover:text-purple-600">
+                        <button className="bg-black w-16 h-16 hover:bg-orange-400 hover:rounded-full transition-transform duration-200 transform hover:scale-90">
+
+                        </button>
+                        <h2 className="text-sm justify-center text-center "> Categoria Categoria</h2>
+                    </div>
+                    <div className="w-16 text-black ease-in duration-200 hover:text-purple-600">
+                        <button className="bg-black w-16 h-16 hover:bg-orange-400 hover:rounded-full transition-transform duration-200 transform hover:scale-90">
+
+                        </button>
+                        <h2 className="text-sm justify-center text-center "> Categoria Categoria</h2>
+                    </div>
+                    <div className="w-16 text-black ease-in duration-200 hover:text-purple-600">
+                        <button className="bg-black w-16 h-16 hover:bg-orange-400 hover:rounded-full transition-transform duration-200 transform hover:scale-90">
+
+                        </button>
+                        <h2 className="text-sm justify-center text-center "> Categoria Categoria</h2>
+                    </div>
+                    
+                </div>
+
+                <div className="flex w-screen flex-row justify-center">
+                    <div className="">
+                        <h3 className="text-black mt-9">
+                            Mais procurados em Beleza e saúde
+                        </h3>
+                        
+                        <div className="flex flex-row flex-nowrap justify-center mt-6 gap-6">
+                            <div className="w-56 h-80 bg-slate-200 text-black">
+                                <div className="w-56 h-40 bg-slate-700"></div>
+                                <div className="ml-4 mt-4">
+                                    <div>
+                                        <h1>
+                                            R$ 75
+                                        </h1>
+                                        <h2> Produtos variados</h2>
+                                    </div>
+                                    <div className="mt-5">
+                                        <img />
+                                        <h3>Dendê - CE</h3>
+                                        <h3>14/10/2023</h3> 
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="w-56 h-80 bg-slate-200 text-black">
+                                <div className="w-56 h-40 bg-slate-700"></div>
+                                <div className="ml-4 mt-4">
+                                    <div>
+                                        <h1>
+                                            R$ 75
+                                        </h1>
+                                        <h2> Produtos variados</h2>
+                                    </div>
+                                    <div className="mt-5">
+                                        <img />
+                                        <h3>Dendê - CE</h3>
+                                        <h3>14/10/2023</h3> 
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="w-56 h-80 bg-slate-200 text-black">
+                                <div className="w-56 h-40 bg-slate-700"></div>
+                                <div className="ml-4 mt-4">
+                                    <div>
+                                        <h1>
+                                            R$ 75
+                                        </h1>
+                                        <h2> Produtos variados</h2>
+                                    </div>
+                                    <div className="mt-5">
+                                        <img />
+                                        <h3>Dendê - CE</h3>
+                                        <h3>14/10/2023</h3> 
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="w-56 h-80 bg-slate-200 text-black">
+                                <div className="w-56 h-40 bg-slate-700"></div>
+                                <div className="ml-4 mt-4">
+                                    <div>
+                                        <h1>
+                                            R$ 75
+                                        </h1>
+                                        <h2> Produtos variados</h2>
+                                    </div>
+                                    <div className="mt-5">
+                                        <img />
+                                        <h3>Dendê - CE</h3>
+                                        <h3>14/10/2023</h3> 
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="w-56 h-80 bg-slate-200 text-black">
+                                <div className="w-56 h-40 bg-slate-700"></div>
+                                <div className="ml-4 mt-4">
+                                    <div>
+                                        <h1>
+                                            R$ 75
+                                        </h1>
+                                        <h2> Produtos variados</h2>
+                                    </div>
+                                    <div className="mt-5">
+                                        <img />
+                                        <h3>Dendê - CE</h3>
+                                        <h3>14/10/2023</h3> 
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="w-56 h-80 bg-slate-200 text-black">
+                                <div className="w-56 h-40 bg-slate-700"></div>
+                                <div className="ml-4 mt-4">
+                                    <div>
+                                        <h1>
+                                            R$ 75
+                                        </h1>
+                                        <h2> Produtos variados</h2>
+                                    </div>
+                                    <div className="mt-5">
+                                        <img />
+                                        <h3>Dendê - CE</h3>
+                                        <h3>14/10/2023</h3> 
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+                <div className="flex w-screen flex-row justify-center">
+                    <div className="">
+                        <h3 className="text-black mt-9">
+                            Mais procurados em Beleza e saúde
+                        </h3>
+                        
+                        <div className="flex flex-row flex-nowrap justify-center mt-6 gap-6">
+                            <div className="w-56 h-80 bg-slate-200 text-black">
+                                <div className="w-56 h-40 bg-slate-700"></div>
+                                <div className="ml-4 mt-4">
+                                    <div>
+                                        <h1>
+                                            R$ 75
+                                        </h1>
+                                        <h2> Produtos variados</h2>
+                                    </div>
+                                    <div className="mt-5">
+                                        <img />
+                                        <h3>Dendê - CE</h3>
+                                        <h3>14/10/2023</h3> 
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="w-56 h-80 bg-slate-200 text-black">
+                                <div className="w-56 h-40 bg-slate-700"></div>
+                                <div className="ml-4 mt-4">
+                                    <div>
+                                        <h1>
+                                            R$ 75
+                                        </h1>
+                                        <h2> Produtos variados</h2>
+                                    </div>
+                                    <div className="mt-5">
+                                        <img />
+                                        <h3>Dendê - CE</h3>
+                                        <h3>14/10/2023</h3> 
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="w-56 h-80 bg-slate-200 text-black">
+                                <div className="w-56 h-40 bg-slate-700"></div>
+                                <div className="ml-4 mt-4">
+                                    <div>
+                                        <h1>
+                                            R$ 75
+                                        </h1>
+                                        <h2> Produtos variados</h2>
+                                    </div>
+                                    <div className="mt-5">
+                                        <img />
+                                        <h3>Dendê - CE</h3>
+                                        <h3>14/10/2023</h3> 
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="w-56 h-80 bg-slate-200 text-black">
+                                <div className="w-56 h-40 bg-slate-700"></div>
+                                <div className="ml-4 mt-4">
+                                    <div>
+                                        <h1>
+                                            R$ 75
+                                        </h1>
+                                        <h2> Produtos variados</h2>
+                                    </div>
+                                    <div className="mt-5">
+                                        <img />
+                                        <h3>Dendê - CE</h3>
+                                        <h3>14/10/2023</h3> 
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="w-56 h-80 bg-slate-200 text-black">
+                                <div className="w-56 h-40 bg-slate-700"></div>
+                                <div className="ml-4 mt-4">
+                                    <div>
+                                        <h1>
+                                            R$ 75
+                                        </h1>
+                                        <h2> Produtos variados</h2>
+                                    </div>
+                                    <div className="mt-5">
+                                        <img />
+                                        <h3>Dendê - CE</h3>
+                                        <h3>14/10/2023</h3> 
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="w-56 h-80 bg-slate-200 text-black">
+                                <div className="w-56 h-40 bg-slate-700"></div>
+                                <div className="ml-4 mt-4">
+                                    <div>
+                                        <h1>
+                                            R$ 75
+                                        </h1>
+                                        <h2> Produtos variados</h2>
+                                    </div>
+                                    <div className="mt-5">
+                                        <img />
+                                        <h3>Dendê - CE</h3>
+                                        <h3>14/10/2023</h3> 
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+            </main>
+        </>
+    );
+}
+
+export default Home;
--- hackathon-hora-do-pesadelo/src/router/router.tsx
+++ hackathon-hora-do-pesadelo/src/router/router.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Product from "../pages/product";
+import Home from "../pages/home";
 
 function Router() {
     return (
         <>
             <BrowserRouter>
                 <Routes>
-                    <Route element={<Product/>} path=""/>
+                    <Route element={<Product/>} path="/product"/>
+                    <Route element={<Home/>} path="/"/>
                 </Routes>
             </BrowserRouter>
         </>
